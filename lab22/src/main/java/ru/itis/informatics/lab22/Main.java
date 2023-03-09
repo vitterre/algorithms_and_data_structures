0ba701799e2650735cd85fa6c8bf54cfdc13f8df
vitterre <bagatovbb@gmail.com>
2023-03-09 20:04:32 +0300

    removed useless class

--- lab22/src/main/java/ru/itis/informatics/lab22/Main.java
+++ lab22/src/main/java/ru/itis/informatics/lab22/Main.java
@@ -1,8 +1,6 @@
 package ru.itis.informatics.lab22;
 
 import ru.itis.informatics.lab22.analyzer.TripAnalyzer;
-import ru.itis.informatics.lab22.analyzer.filter.TripFilter;
-import ru.itis.informatics.lab22.analyzer.filter.other.DepartureTimeFilter;
 import ru.itis.informatics.lab22.domain.Trip;
 import ru.itis.informatics.lab22.processor.TripProcessor;
 
@@ -13,19 +11,16 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		List<String> lines = Files.readAllLines(Paths.get("lab22/src/main/resources/input.csv"));
+		final List<String> lines = Files.readAllLines(Paths.get("lab22/src/main/resources/input.csv"));
 
 		final TripProcessor tripProcessor = new TripProcessor();
 
-		List<Trip> trips = tripProcessor.getTripsFromLines(lines);
+		final List<Trip> trips = tripProcessor.getTripsFromLines(lines);
 
-		List<Trip> result = new TripAnalyzer().getFilteredTrips(trips, new TripFilter() {
-			@Override
-			public boolean compare(Trip one, Trip another) {
-				return one.getDetails().getDepartureTime()
-								.equals(another.getDetails().getDepartureTime());
-			}
-		});
+		final List<Trip> result = new TripAnalyzer().getFilteredTrips(
+						trips,
+						(one, another) -> one.getDetails().getDepartureTime()
+						                  .equals(another.getDetails().getDepartureTime()));
 
 		result.forEach(System.out::println);
 	}
